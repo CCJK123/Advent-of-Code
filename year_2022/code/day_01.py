43a727311ccaa997267b9034107bb0c973ae2e8f
Christopher Cheng <chris.chengjk@gmail.com>
2022-12-01 13:18:13 +0800

    Add AoC 2022 Day 1

--- year_2022/code/day_01.py
+++ year_2022/code/day_01.py
@@ -1,2 +1,15 @@
 def day_01(input_str):
-    pass
+    # Inital setup
+    input_array = [sum([int(j) for j in i.split("\n")])
+                   for i in input_str.split("\n\n")]
+    outputs = []
+
+    # Part 1
+    outputs.append(max(input_array))
+
+    # Part 2
+    temp_array = input_array.copy()
+    temp_array.sort()
+    outputs.append(sum(temp_array[-3:]))
+
+    return outputs
