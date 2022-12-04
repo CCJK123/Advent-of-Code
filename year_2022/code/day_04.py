71f9fa2c6517fe47b2f8522b1498afe7e8a29832
Christopher Cheng <chris.chengjk@gmail.com>
2022-12-04 18:16:45 +0800

    Add AoC 2022 Day 4

---
+++ year_2022/code/day_04.py
@@ -0,0 +1,26 @@
+def day_04(input_str):
+    # Inital setup
+    input_array = [[j.split("-") for j in i.split(",")]
+                   for i in input_str.split("\n")]
+    for i in range(len(input_array)):
+        for j in range(2):
+            input_array[i][j] = set(
+                range(int(input_array[i][j][0]), int(input_array[i][j][1])+1)
+            )
+    outputs = []
+
+    # Part 1
+    count = 0
+    for pair in input_array:
+        if pair[0].issubset(pair[1]) or pair[1].issubset(pair[0]):
+            count += 1
+    outputs.append(count)
+
+    # Part 2
+    count = 0
+    for pair in input_array:
+        if pair[0].intersection(pair[1]) != set():
+            count += 1
+    outputs.append(count)
+
+    return outputs
