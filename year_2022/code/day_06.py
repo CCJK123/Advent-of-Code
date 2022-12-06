c43feeaa820adb73be1b7626d89f96e8b65f383a
Christopher Cheng <chris.chengjk@gmail.com>
2022-12-06 13:50:55 +0800

    Added AoC 2022 Day 6

---
+++ year_2022/code/day_06.py
@@ -0,0 +1,17 @@
+def day_06(input_str):
+    # Inital setup
+    outputs = []
+
+    # Part 1
+    for i in range(len(input_str)-4+1):
+        if len(set(input_str[i:i+4])) == len(input_str[i:i+4]):
+            outputs.append(i+4)
+            break
+
+    # Part 2
+    for i in range(len(input_str)-14+1):
+        if len(set(input_str[i:i+14])) == len(input_str[i:i+14]):
+            outputs.append(i+14)
+            break
+
+    return outputs
