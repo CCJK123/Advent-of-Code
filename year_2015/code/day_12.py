d9bd2ca67cc98245d22b8e62ca4a72cff9172882
Christopher Cheng <39285519+CCJK123@users.noreply.github.com>
2024-05-14 15:38:28 +0800

    Added AoC 2015 Day 12

---
+++ year_2015/code/day_12.py
@@ -0,0 +1,35 @@
+def day_12(input_str):
+    # Initial setup
+    import json, re
+    input_json = json.loads(input_str)
+    outputs = []
+
+    # Port 1
+    num_sum = 0
+    for match_obj in re.finditer(r"-?\d+", input_str):
+        num_sum += int(match_obj.group(0))
+    outputs.append(num_sum)
+
+    # Part 2
+    def parse(obj):
+        match obj:
+            case int():
+                return obj
+            case str():
+                if obj == "red":
+                    return None
+                return 0
+            case list():
+                sub_num_sum = 0
+                for sub_obj in obj:
+                    temp = parse(sub_obj)
+                    if temp is not None:
+                        sub_num_sum += temp
+                return sub_num_sum
+            case dict():
+                if "red" not in obj.values():
+                    return parse(list(obj.values()))
+                return 0
+    outputs.append(parse(input_json))
+    
+    return outputs
