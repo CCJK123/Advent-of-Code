97a74447021665cd1f8dc3817afb8b72e67240b0
Christopher Cheng <chris.chengjk@gmail.com>
2024-12-05 18:42:37 +0800

    Add AoC 2024 Day 4 Part 2 (Rust)

--- solutions/rust/src/y2024/d04.rs
+++ solutions/rust/src/y2024/d04.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use std::error::Error;
 
 pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
@@ -15,7 +16,12 @@ pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
             match row.find("X") {
                 Some(col_offset) => {
                     col_index += col_offset;
-                    x_results.append(&mut search('M', &input, [row_index, col_index], None));
+                    x_results.append(&mut search_adjacent(
+                        'M',
+                        &input,
+                        [row_index, col_index],
+                        None,
+                    ));
                     row = &row[col_offset + 1..];
                     col_index += 1;
                 }
@@ -26,19 +32,61 @@ pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
     // Get 'A's from 'M's
     let mut m_results: Vec<([usize; 2], Option<[i16; 2]>)> = Vec::new();
     for (center_coords, direction) in x_results {
-        m_results.append(&mut search('A', &input, center_coords, direction))
+        m_results.append(&mut search_adjacent('A', &input, center_coords, direction))
     }
     // Get 'S's from "A"s
     let mut a_results: Vec<([usize; 2], Option<[i16; 2]>)> = Vec::new();
     for (center_coords, direction) in m_results {
-        a_results.append(&mut search('S', &input, center_coords, direction))
+        a_results.append(&mut search_adjacent('S', &input, center_coords, direction))
     }
     outputs.push(a_results.len());
+
+    // Part 2
+    let mut coords_vec: Vec<[usize; 2]> = Vec::new();
+    for (row_index, &row) in input.iter().enumerate() {
+        let mut row = row;
+        let mut col_index = 0;
+        loop {
+            match row.find("A") {
+                Some(col_offset) => {
+                    col_index += col_offset;
+                    coords_vec.push([row_index, col_index]);
+                    row = &row[col_offset + 1..];
+                    col_index += 1;
+                }
+                None => break,
+            }
+        }
+    }
+    let mut count = 0;
+    for coords in coords_vec {
+        if coords.iter().any(|c| *c <= 0)
+            || coords[0] as usize >= input.len() - 1
+            || coords[1] as usize >= input[0].len() - 1
+        {
+            continue;
+        }
+        for arrangement in "MMSS"
+            .chars()
+            .cycle()
+            .tuple_windows::<(_, _, _, _)>()
+            .take(4)
+        {
+            if arrangement.0 == input[coords[0] + 1].chars().nth(coords[1] + 1).unwrap()
+                && arrangement.1 == input[coords[0] + 1].chars().nth(coords[1] - 1).unwrap()
+                && arrangement.2 == input[coords[0] - 1].chars().nth(coords[1] - 1).unwrap()
+                && arrangement.3 == input[coords[0] - 1].chars().nth(coords[1] + 1).unwrap()
+            {
+                count += 1
+            }
+        }
+    }
+    outputs.push(count);
 
     Ok(outputs.iter().map(|s| s.to_string()).collect())
 }
 
-fn search(
+fn search_adjacent(
     letter: char,
     input: &Vec<&str>,
     center_coords: [usize; 2],
