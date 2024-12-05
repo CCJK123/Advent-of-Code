e288a503c68e5edf2a6440b618656c9e003d1c85
Christopher Cheng <chris.chengjk@gmail.com>
2024-12-05 14:36:37 +0800

    Add AoC 2024 Day 3 Part 2 (Rust)

--- solutions/rust/src/y2024/d03.rs
+++ solutions/rust/src/y2024/d03.rs
@@ -6,15 +6,29 @@ pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let mut outputs = Vec::new();
 
     // Part 1
+    outputs.push(get_mul_total(input));
+
+    // Part 2
     let mut total = 0;
-    for (_, [no_1, no_2]) in Regex::new(r"mul\((\d+),(\d+)\)")
+    for capture in Regex::new(r"(?s)do\(\).*?don't\(\)")
         .unwrap()
-        .captures_iter(input)
-        .map(|c| c.extract())
+        .captures_iter(&format!("do(){input}don't()"))
     {
-        total += no_1.parse::<u32>().unwrap() * no_2.parse::<u32>().unwrap();
+        total += get_mul_total(capture.get(0).unwrap().as_str());
     }
     outputs.push(total);
 
     Ok(outputs.iter().map(|s| s.to_string()).collect())
+}
+
+fn get_mul_total(haystack: &str) -> u32 {
+    let mut total = 0;
+    for (_, [no_1, no_2]) in Regex::new(r"mul\((\d+),(\d+)\)")
+        .unwrap()
+        .captures_iter(haystack)
+        .map(|c| c.extract())
+    {
+        total += no_1.parse::<u32>().unwrap() * no_2.parse::<u32>().unwrap();
+    }
+    total
 }
