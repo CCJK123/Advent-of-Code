2eb8eb82ba057023fc68ecc55f5b0ce868a615cb
Christopher Cheng <chris.chengjk@gmail.com>
2024-12-05 09:21:30 +0800

    Add AoC 2024 Day 2 Part 2 (Rust)

--- solutions/rust/src/y2024/d02.rs
+++ solutions/rust/src/y2024/d02.rs
@@ -6,30 +6,39 @@ pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
 
     // Part 1
     let mut safe_count = 0;
+    let mut safe_count_tolerant = 0;
     for report in input.lines() {
-        let mut levels = report.split(" ").map(|n| n.parse::<u8>().unwrap());
-        let mut level_old = levels.next().unwrap();
-        let mut is_increasing = None;
-        let mut is_safe = true;
-
-        for level_new in levels {
-            let safety_results = check_safety(level_old, level_new, is_increasing);
-            is_increasing = safety_results.1;
-            is_safe = is_safe && safety_results.0;
-            level_old = level_new
-        }
-        if is_safe {
-            safe_count += 1
+        if check_report_safety(
+            report
+                .split(" ")
+                .map(|n| n.parse::<u8>().unwrap())
+                .collect(),
+        ) {
+            safe_count += 1;
+            safe_count_tolerant += 1;
+        } else {
+            // Part 2
+            let levels: Vec<u8> = report
+                .split(" ")
+                .map(|n| n.parse::<u8>().unwrap())
+                .collect();
+            for i in 0..levels.len() {
+                if check_report_safety([&levels[..i], &levels[i + 1..]].concat()) {
+                    safe_count_tolerant += 1;
+                    break;
+                }
+            }
         }
     }
     outputs.push(safe_count);
+    outputs.push(safe_count_tolerant);
 
     Ok(outputs.iter().map(|s| s.to_string()).collect())
 }
 
-fn check_safety(
-    level_old: u8,
-    level_new: u8,
+fn check_pair_safety(
+    level_old: &u8,
+    level_new: &u8,
     mut is_increasing: Option<bool>,
 ) -> (bool, Option<bool>) {
     // Check all increasing/decreasing
@@ -44,9 +53,23 @@ fn check_safety(
         None => is_increasing = Some(level_new > level_old),
     }
     // Check 1 <= diff <= 3
-    let diff = level_old.abs_diff(level_new);
+    let diff = level_old.abs_diff(*level_new);
     if diff < 1 || diff > 3 {
         return (false, is_increasing);
     }
     return (true, is_increasing);
+}
+
+fn check_report_safety(levels: Vec<u8>) -> bool {
+    let mut levels = levels.iter();
+    let mut level_old = levels.next().unwrap();
+    let mut is_increasing = None;
+    let mut is_safe = true;
+    for level_new in levels {
+        let safety_results = check_pair_safety(level_old, level_new, is_increasing);
+        is_increasing = safety_results.1;
+        is_safe = is_safe && safety_results.0;
+        level_old = level_new
+    }
+    is_safe
 }
