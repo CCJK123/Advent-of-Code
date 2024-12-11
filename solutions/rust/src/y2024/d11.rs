db202a720ac348470d380770b6bf475d53c22733
Christopher Cheng <chris.chengjk@gmail.com>
2024-12-11 20:25:26 +0800

    Add AoC 2024 Day 11 Part 2 (Rust)

--- solutions/rust/src/y2024/d11.rs
+++ solutions/rust/src/y2024/d11.rs
@@ -1,4 +1,4 @@
-use std::error::Error;
+use std::{collections::HashMap, error::Error};
 
 pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
     // Initial setup
@@ -8,36 +8,41 @@ pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
         .map(|s| s.parse::<u64>().unwrap())
         .collect::<Vec<_>>();
 
-    // Part 1
-    let mut stones = input;
-    for _ in 0..25 {
-        stones = stones
-            .iter()
-            .map(|stone| {
-                let mut result = Vec::new();
-                if *stone == 0 {
-                    result.push(1);
-                } else if stone.checked_ilog10().unwrap_or(0) % 2 == 1 {
-                    let stone_string = stone.to_string();
-                    let (left, right) = stone_string.split_at(
-                        ((stone.checked_ilog10().unwrap() + 1) / 2)
-                            .try_into()
-                            .unwrap(),
-                    );
-                    result.push(left.parse::<u64>().unwrap());
-                    result.push(right.parse::<u64>().unwrap());
-                } else {
-                    result.push(stone * 2024)
-                }
-                result
-            })
-            .reduce(|mut acc, e| {
-                acc.extend(e);
-                acc
-            })
-            .unwrap();
+    // Part 1 & 2 combined
+    let mut stones = input.iter().fold(HashMap::new(), |mut acc, &s| {
+        *acc.entry(s).or_insert(0) += 1;
+        acc
+    });
+    let mut transformations = HashMap::<u64, Vec<u64>>::from([(0, vec![1])]);
+    for blink_no in 0..75 {
+        let mut stones_next = HashMap::new();
+        for (stone, count) in stones.iter() {
+            if !transformations.contains_key(stone) {
+                transformations.insert(*stone, {
+                    if stone.checked_ilog10().unwrap_or(0) % 2 == 1 {
+                        let stone_string = stone.to_string();
+                        let (left, right) = stone_string.split_at(
+                            ((stone.checked_ilog10().unwrap() + 1) / 2)
+                                .try_into()
+                                .unwrap(),
+                        );
+                        vec![left.parse::<u64>().unwrap(), right.parse::<u64>().unwrap()]
+                    } else {
+                        vec![stone * 2024]
+                    }
+                });
+            }
+            for &child_stone in transformations[stone].iter() {
+                *stones_next.entry(child_stone).or_insert(0) += count;
+            }
+        }
+        stones = stones_next;
+        // Part 1
+        if blink_no == 24 {
+            outputs.push(stones.values().sum::<u64>());
+        }
     }
-    outputs.push(stones.len());
+    outputs.push(stones.values().sum()); // Part 2
 
     Ok(outputs.iter().map(|s| s.to_string()).collect())
 }
