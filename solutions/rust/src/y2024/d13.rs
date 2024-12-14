4ef297abc8b87ef04bd6b4da8f6fb2320c5fb749
Christopher Cheng <chris.chengjk@gmail.com>
2024-12-14 10:40:06 +0800

    Add AoC 2024 Day 13 Part 2 (Rust)

--- solutions/rust/src/y2024/d13.rs
+++ solutions/rust/src/y2024/d13.rs
@@ -20,36 +20,55 @@ pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
         })
         .into_iter()
         .map(|v| [v[0], v[1], v[2]])
-        .collect::<Vec<[[u32; 2]; 3]>>();
+        .collect::<Vec<[[i64; 2]; 3]>>();
 
     // Part 1
-    let mut tokens = 0;
-    for [button_a, button_b, prize] in &input {
-        let mut token_options = Vec::new();
-        let max_a_count = (prize[0] / button_a[0])
-            .min(prize[1] / button_a[1])
-            .min(100);
-        let max_b_count = (prize[0] / button_b[0])
-            .min(prize[1] / button_b[1])
-            .min(100);
-        for a_count in 0..=max_a_count {
-            for b_count in 0..=max_b_count {
-                if *prize
-                    == [
-                        button_a[0] * a_count + button_b[0] * b_count,
-                        button_a[1] * a_count + button_b[1] * b_count,
-                    ]
-                {
-                    token_options.push(a_count * 3 + b_count);
-                    break;
-                }
-            }
-        }
-        if let Some(min_tokens) = token_options.iter().min() {
-            tokens += min_tokens;
-        }
-    }
-    outputs.push(tokens);
+    outputs.push(calculate_tokens(&input));
+
+    // Part 2
+    let machines = input
+        .iter()
+        .map(|[a, b, [p_x, p_y]]| [*a, *b, [p_x + 10000000000000, p_y + 10000000000000]])
+        .collect::<Vec<_>>();
+    outputs.push(calculate_tokens(&machines));
 
     Ok(outputs.iter().map(|s| s.to_string()).collect())
+}
+
+/// **Formula Derivation**
+///
+/// Let the input coordinates for button A, button B and prize be (a_x, a_y),
+/// (b_x, b_y) and (p_x, p_y) respectively.
+/// Let A and B be the number of times buttons A and B are pressed respectively.
+///
+/// To solve: `A*a_x + B*b_x = p_x (1)` and `A*a_y + B*b_y = p_y (2)`
+///
+/// `(1) * b_y`: `A*a_x*b_y + B*b_x*b_y = p_x*b_y` => `B*b_x*b_y = p_x*b_y - A*a_x*b_y (3)`
+///
+/// `(2) * b_x`: `A*a_y*b_x + B*b_x*b_y = p_y*b_x` => `B*b_x*b_y = p_y*b_x - A*a_y*b_x (4)`
+///
+/// `(3) = (4)`: `p_x*b_y - A*a_x*b_y = p_y*b_x - A*a_y*b_x`
+///           => `A * (a_x*b_y - a_y*b_x) = p_x*b_y - p_y*b_x`
+///
+/// ∴ `A = (p_x*b_y - p_y*b_x) / (a_x*b_y - a_y*b_x)`
+///
+/// Similarly for B, `∴ B = (p_x*a_y - p_y*a_x) / (b_x*a_y - b_y*a_x) = (p_y*a_x - p_x*a_y) / (a_x*b_y - a_y*b_x)`
+///
+/// (Alternatively, use [Cramer's rule](https://en.wikipedia.org/wiki/Cramer's_rule)
+/// directly instead of deriving the formula yourself, to obtain solutions for A and B)
+fn calculate_tokens(input: &Vec<[[i64; 2]; 3]>) -> i64 {
+    let mut tokens = 0;
+    for [button_a, button_b, prize] in input {
+        let a_numerator = prize[0] * button_b[1] - prize[1] * button_b[0];
+        let b_numerator = prize[1] * button_a[0] - prize[0] * button_a[1];
+        let denominator = button_a[0] * button_b[1] - button_a[1] * button_b[0];
+        assert_ne!(denominator, 0); // Assume pair of linear equations has one unique solution
+        if a_numerator % denominator == 0 && b_numerator % denominator == 0 {
+            // Integer solutions exist for pair of linear equations, hence prize is winnable
+            let a_count = a_numerator / denominator;
+            let b_count = b_numerator / denominator;
+            tokens += 3 * a_count + b_count;
+        }
+    }
+    tokens
 }
