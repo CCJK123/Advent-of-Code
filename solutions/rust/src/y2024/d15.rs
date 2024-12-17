836d32d7e0e2eceb114f4fbbb9c727086d4a3411
Christopher Cheng <chris.chengjk@gmail.com>
2024-12-17 09:59:29 +0800

    Add AoC 2024 Day 15 Part 2 (Rust)

--- solutions/rust/src/y2024/d15.rs
+++ solutions/rust/src/y2024/d15.rs
@@ -20,11 +20,6 @@ pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
             .collect::<Vec<_>>(),
         input[1].join(""),
     );
-
-    // Part 1
-    let walls = get_char_coords(&input, '#');
-    let mut boxes = get_char_coords(&input, 'O');
-    let mut robot = get_char_coords(&input, '@').into_iter().next().unwrap();
     let moves = input
         .1
         .chars()
@@ -37,21 +32,20 @@ pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
             }
             acc
         });
-    for (direction_char, step_count) in moves {
-        let direction = match direction_char {
-            '^' => [-1, 0],
-            'v' => [1, 0],
-            '<' => [0, -1],
-            '>' => [0, 1],
-            _ => panic!(),
-        };
-        let wall = get_closest_linear_obstacle(&walls, direction_char, &robot)
+
+    // Part 1
+    let walls = get_char_coords(&input, '#');
+    let mut boxes = get_char_coords(&input, 'O');
+    let mut robot = get_char_coords(&input, '@').into_iter().next().unwrap();
+    for &(direction_char, step_count) in &moves {
+        let direction = get_direction(direction_char);
+        let wall = get_closest_linear_obstacles(&walls, direction_char, &robot)
             .next()
             .unwrap();
         if manhattan_distance(&robot, wall) == 1 {
             continue; // Wall directly blocking movement
         }
-        let current_boxes = get_closest_linear_obstacle(&boxes, direction_char, &robot)
+        let current_boxes = get_closest_linear_obstacles(&boxes, direction_char, &robot)
             .filter(|&&b| robot.min(*wall) < b && b < robot.max(*wall))
             .rev()
             .collect::<Vec<_>>();
@@ -70,9 +64,66 @@ pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
             }
         }
     }
-
     let mut gps_coord_sum = 0;
     for [x, y] in &boxes {
+        gps_coord_sum += 100 * x + y
+    }
+    outputs.push(gps_coord_sum);
+
+    // Part 2
+    let walls = get_char_coords(&input, '#')
+        .into_iter()
+        .map(|[x, y]| [[x, y * 2], [x, y * 2 + 1]])
+        .flatten()
+        .collect::<HashSet<_>>();
+    let mut boxes = get_char_coords(&input, 'O')
+        .into_iter()
+        .map(|[x, y]| [[x, y * 2], [x, y * 2 + 1]])
+        .collect::<HashSet<_>>();
+    let mut robot = get_char_coords(&input, '@')
+        .into_iter()
+        .map(|[x, y]| [x, y * 2])
+        .next()
+        .unwrap();
+    for &(direction_char, step_count) in &moves {
+        // Assert no overlapping boxes (was for debugging)
+        assert!(boxes.iter().all(|&[l, r]| boxes
+            .iter()
+            .all(|&b| { b == [l, r] || b.iter().all(|&s| s != l && s != r) })));
+
+        let direction = get_direction(direction_char);
+        let wall = get_closest_linear_obstacles(&walls, direction_char, &robot)
+            .next()
+            .unwrap();
+        if manhattan_distance(&robot, wall) == 1 {
+            continue; // Wall directly blocking movement
+        }
+        let (box_changes, robot_step_count) =
+            propagate_wide_box_movement(&robot, &walls, &boxes, direction_char, step_count);
+        let old_robot = robot;
+        robot = move_steps(&robot, direction, robot_step_count);
+        boxes = boxes
+            .into_iter()
+            .map(|old_box| {
+                let relevant_change = box_changes
+                    .iter()
+                    .filter(|&&(side, _, _)| {
+                        side != old_robot && (old_box[0] == side || old_box[1] == side)
+                    })
+                    .next();
+                if let Some(&(_, direction, step_count)) = relevant_change {
+                    [
+                        move_steps(&old_box[0], direction, step_count),
+                        move_steps(&old_box[1], direction, step_count),
+                    ]
+                } else {
+                    old_box
+                }
+            })
+            .collect::<HashSet<_>>();
+    }
+    let mut gps_coord_sum = 0;
+    for [[x, y], _] in &boxes {
         gps_coord_sum += 100 * x + y
     }
     outputs.push(gps_coord_sum);
@@ -99,11 +150,21 @@ fn get_char_coords(input: &(Vec<Vec<char>>, String), character: char) -> HashSet
         .collect()
 }
 
+fn get_direction(direction_char: char) -> [i32; 2] {
+    match direction_char {
+        '^' => [-1, 0],
+        'v' => [1, 0],
+        '<' => [0, -1],
+        '>' => [0, 1],
+        _ => panic!(),
+    }
+}
+
 fn manhattan_distance(a: &[i32; 2], b: &[i32; 2]) -> u32 {
     a[0].abs_diff(b[0]) + a[1].abs_diff(b[1])
 }
 
-fn get_closest_linear_obstacle<'a>(
+fn get_closest_linear_obstacles<'a>(
     obstacles: &'a HashSet<[i32; 2]>,
     direction_char: char,
     robot: &[i32; 2],
@@ -154,4 +215,153 @@ fn propagate_movement(
     let step_count = base_step_count + additional_step_count;
     moved_boxes.push(move_steps(reference, direction, step_count));
     (moved_boxes, step_count)
+}
+
+fn get_closest_wide_box<'a>(
+    boxes: &'a HashSet<[[i32; 2]; 2]>,
+    direction_char: char,
+    robot: &[i32; 2],
+) -> Option<&'a [[i32; 2]; 2]> {
+    boxes
+        .iter()
+        .filter(|b| match direction_char {
+            '^' => b
+                .iter()
+                .any(|side| side[0] < robot[0] && side[1] == robot[1]),
+            'v' => b
+                .iter()
+                .any(|side| side[0] > robot[0] && side[1] == robot[1]),
+            '<' => b[0][0] == robot[0] && b[0][1] < robot[1],
+            '>' => b[0][0] == robot[0] && b[0][1] > robot[1],
+            _ => panic!(),
+        })
+        .sorted_by_key(|b| manhattan_distance(&b[0], robot).min(manhattan_distance(&b[1], robot)))
+        .next()
+}
+
+fn propagate_wide_box_movement(
+    reference: &[i32; 2],
+    walls: &HashSet<[i32; 2]>,
+    boxes: &HashSet<[[i32; 2]; 2]>,
+    direction_char: char,
+    max_step_count: u32,
+) -> (HashSet<([i32; 2], [i32; 2], u32)>, u32) {
+    let direction = get_direction(direction_char);
+    let wall = get_closest_linear_obstacles(walls, direction_char, reference)
+        .next()
+        .unwrap();
+    match get_closest_wide_box(&boxes, direction_char, reference) {
+        None => {
+            let step_count = max_step_count.min(manhattan_distance(reference, wall) - 1);
+            (
+                HashSet::from([(*reference, direction, step_count)]),
+                step_count,
+            )
+        }
+        Some(closest_box) => {
+            // Handle cases where wall between box and reference
+            // OR reference cannot reach closest box
+            let distance_to_closest_box = manhattan_distance(&closest_box[0], reference)
+                .min(manhattan_distance(&closest_box[1], reference));
+            if manhattan_distance(wall, reference) < distance_to_closest_box
+                || max_step_count < distance_to_closest_box
+            {
+                let step_count = max_step_count.min(manhattan_distance(reference, wall) - 1);
+                return (
+                    HashSet::from([(*reference, direction, step_count)]),
+                    step_count,
+                );
+            }
+
+            match direction_char {
+                '^' | 'v' => {
+                    let base_step_count = max_step_count
+                        .min(manhattan_distance(&closest_box[0], reference) - 1)
+                        .min(manhattan_distance(&closest_box[1], reference) - 1);
+                    let [left, right] = closest_box;
+                    let side_step_count = max_step_count - base_step_count;
+                    let (mut left_box_changes, left_step_count) = propagate_wide_box_movement(
+                        left,
+                        walls,
+                        boxes,
+                        direction_char,
+                        side_step_count,
+                    );
+                    let (mut right_box_changes, right_step_count) = propagate_wide_box_movement(
+                        right,
+                        walls,
+                        boxes,
+                        direction_char,
+                        side_step_count,
+                    );
+                    // Move the maximum amount possible that both sides can move
+                    if left_step_count > right_step_count {
+                        (left_box_changes, _) = propagate_wide_box_movement(
+                            left,
+                            walls,
+                            boxes,
+                            direction_char,
+                            right_step_count,
+                        );
+                    } else if right_step_count > left_step_count {
+                        (right_box_changes, _) = propagate_wide_box_movement(
+                            right,
+                            walls,
+                            boxes,
+                            direction_char,
+                            left_step_count,
+                        );
+                    }
+                    let step_count = base_step_count + left_step_count.min(right_step_count);
+                    let mut box_changes =
+                        left_box_changes.union(&right_box_changes).map(|b| *b).fold(
+                            HashSet::<([i32; 2], [i32; 2], u32)>::new(),
+                            |mut acc, (side, dir, new_step)| {
+                                let similar_change = acc
+                                    .iter()
+                                    .filter(|&&(s, d, _)| s == side && d == dir)
+                                    .next();
+                                match similar_change {
+                                    Some(&(_, _, old_step)) => {
+                                        // Ensure each box moves as far as possible
+                                        // (different side branches can move later boxes different amounts)
+                                        // (e.g. if only one side has air gaps between current box and later box)
+                                        if new_step > old_step {
+                                            acc.remove(&(side, dir, old_step));
+                                            acc.insert((side, dir, new_step));
+                                        }
+                                    }
+                                    None => {
+                                        acc.insert((side, dir, new_step));
+                                    }
+                                }
+                                acc
+                            },
+                        );
+                    box_changes.insert((*reference, direction, step_count));
+                    (box_changes, step_count)
+                }
+                '<' | '>' => {
+                    let [close, far] = if direction_char == '<' {
+                        [closest_box[1], closest_box[0]]
+                    } else {
+                        *closest_box
+                    };
+                    let base_step_count =
+                        max_step_count.min(manhattan_distance(&close, reference) - 1);
+                    let (mut box_changes, additional_step_count) = propagate_wide_box_movement(
+                        &far,
+                        walls,
+                        boxes,
+                        direction_char,
+                        max_step_count - base_step_count,
+                    );
+                    let step_count = base_step_count + additional_step_count;
+                    box_changes.insert((*reference, direction, step_count));
+                    (box_changes, step_count)
+                }
+                _ => panic!(),
+            }
+        }
+    }
 }
