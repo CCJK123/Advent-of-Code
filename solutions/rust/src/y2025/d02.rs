e247c2668cd8bdaab6af1eeb653559d0f3d8c81e
Christopher Cheng <chris@ccjk.dev>
2025-12-10 16:47:00 +0800

    Add AoC 2025 Day 2 Part 2 (Rust)

--- solutions/rust/src/y2025/d02.rs
+++ solutions/rust/src/y2025/d02.rs
@@ -48,5 +48,51 @@ pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
     }
     outputs.push(sum);
 
+    // Part 2
+    let mut sum = 0;
+    for [lower_bound, upper_bound] in ranges.iter() {
+        let possible_lengths = lower_bound.to_string().len()..=upper_bound.to_string().len();
+
+        for length in possible_lengths {
+            let length = length as u32;
+
+            // Get bounds for a specific length
+            let lower_bound = (*lower_bound).max(10_u64.pow(length - 1));
+            let upper_bound = (*upper_bound).min(10_u64.pow(length) - 1);
+            assert_eq!(lower_bound.to_string().len() as u32, length);
+            assert_eq!(upper_bound.to_string().len() as u32, length);
+
+            // Instantiate variable to store invalid IDs to prevent double-counting
+            let mut invalid_ids = vec![];
+
+            // Get valid repeating sub-IDs and sum accordingly
+            let possible_sub_lengths = (1..=length / 2).filter(|x| length % x == 0);
+            for sub_length in possible_sub_lengths {
+                let lower_bound_chunk: u64 = lower_bound.to_string()[..sub_length as usize]
+                    .parse()
+                    .unwrap();
+                let upper_bound_chunk: u64 = upper_bound.to_string()[..sub_length as usize]
+                    .parse()
+                    .unwrap();
+
+                let multiplier = ("0".repeat(sub_length as usize - 1) + "1")
+                    .repeat((length / sub_length) as usize)
+                    .parse::<u64>()
+                    .unwrap();
+                invalid_ids.extend(
+                    (lower_bound_chunk..=upper_bound_chunk)
+                        .map(|x| x * multiplier)
+                        .filter(|x| {
+                            !invalid_ids.contains(x) && x >= &lower_bound && x <= &upper_bound
+                        })
+                        .collect::<Vec<_>>(),
+                );
+            }
+
+            sum += invalid_ids.iter().sum::<u64>();
+        }
+    }
+    outputs.push(sum);
+
     Ok(outputs.iter().map(|s| s.to_string()).collect())
 }
