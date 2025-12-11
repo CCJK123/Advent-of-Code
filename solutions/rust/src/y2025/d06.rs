24214d09938f7c04d48ea48458ef81b512b8f9cd
Christopher Cheng <chris@ccjk.dev>
2025-12-11 15:44:36 +0800

    Add AoC 2025 Day 6 Part 2 (Rust)

--- solutions/rust/src/y2025/d06.rs
+++ solutions/rust/src/y2025/d06.rs
@@ -1,23 +1,63 @@
 use std::error::Error;
 
+use itertools::Itertools;
+
 pub fn run(input: &str) -> Result<Vec<String>, Box<dyn Error>> {
     // Initial setup
     let mut outputs = Vec::new();
+
+    // Part 1
     let homework = input
         .lines()
         .map(|s| s.split_whitespace().collect::<Vec<_>>())
         .collect::<Vec<_>>();
-
-    // Part 1
     let total = (0..homework[0].len())
-        .map(|i| match homework[homework.len() - 1][i] {
-            "+" => (0..=(homework.len() - 2))
-                .map(|j| homework[j][i].parse::<u64>().unwrap())
-                .sum::<u64>(),
-            "*" => (0..=(homework.len() - 2))
-                .map(|j| homework[j][i].parse::<u64>().unwrap())
-                .product::<u64>(),
-            a => panic!("{a}"),
+        .map(|i| {
+            let numbers =
+                (0..=(homework.len() - 2)).map(|j| homework[j][i].parse::<u64>().unwrap());
+            match homework[homework.len() - 1][i] {
+                "+" => numbers.sum::<u64>(),
+                "*" => numbers.product::<u64>(),
+                _ => panic!(),
+            }
+        })
+        .sum::<u64>();
+    outputs.push(total);
+
+    // Part 2
+    let homework = input
+        .lines()
+        .map(|s| {
+            let mut v = s.chars().collect::<Vec<_>>();
+            v.insert(0, ' ');
+            v
+        })
+        .collect::<Vec<_>>();
+    let mut problems = vec![];
+    for i in 0..homework[0].len() {
+        if ['+', '*'].contains(&homework[homework.len() - 1][i]) {
+            problems.push((homework[homework.len() - 1][i], vec![]));
+        }
+
+        let number = match (0..(homework.len() - 1))
+            .map(|j| homework[j][i])
+            .join("")
+            .trim()
+            .parse::<u64>()
+        {
+            Err(_) => continue,
+            Ok(x) => x,
+        };
+        let problem_count = problems.len();
+        problems[problem_count - 1].1.push(number);
+    }
+
+    let total = problems
+        .iter()
+        .map(|(operator, numbers)| match operator {
+            '+' => numbers.iter().sum::<u64>(),
+            '*' => numbers.iter().product::<u64>(),
+            _ => panic!(),
         })
         .sum::<u64>();
     outputs.push(total);
